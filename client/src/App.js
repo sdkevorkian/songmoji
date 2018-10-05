dea7f4bd65ffcfd4dc783c143f31ec3446089d10
Sara Kevorkian <sdkevorkian@gmail.com>
2018-10-05 11:52:27 -0700

    added bootstrap, initial searchbox

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SongSearchBoxes from './SongSearchBoxes/SongSearchBoxes.js';
 
 class App extends Component {
     state = {
@@ -20,22 +21,13 @@ class App extends Component {
     }
     render() {
         return (
-            <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-                >
-                Learn React
-                </a>
-                </header>
-            <p className="App-intro">{this.state.response}</p>
+            <div className="App container-fluid">
+            <h1 class="title">
+                songmoji    
+            </h1>
+            <p>search for a song and translate the lyrics into emojis!</p>
+            <SongSearchBoxes/>
+            {/*<p className="App-intro">{this.state.response}</p>*/}
             </div>
         );
     }
---
+++ client/src/SongSearchBoxes/SongSearchBoxes.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class SongSearchBoxes extends React.Component {
+    render() {
+        return (
+            <div className="form-group"><label>test</label>
+                <input className="form-control"/>
+            </div>
+            )
+    }
+}
+
+export default SongSearchBoxes;
