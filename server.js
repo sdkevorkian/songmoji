047446b2da04bab3c43e368ea14cf92f75eab4e7
Sara Kevorkian <sdkevorkian@gmail.com>
2018-10-08 15:33:12 -0700

    heroku ready

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const emojify = require('moji-translate');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,5 +15,14 @@ app.post('/api/emoji', (req, res) => {
     var lyrics = req.body.lyrics;
     res.send({ emojis: emojify.translate(lyrics) });
 });
+
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+    // Handle React routing, return all requests to React app
+    app.get('*', function (req, res) {
+        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
